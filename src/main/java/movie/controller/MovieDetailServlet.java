40b1b2dae4d9367fad41a25685299a2ee0f3b405
YS_LABTOP\syski <sysking1009@naver.com>
2026-05-07 17:21:44 +0900

    유니코드 오류 문자 수정

--- src/main/java/movie/controller/MovieDetailServlet.java
+++ src/main/java/movie/controller/MovieDetailServlet.java
@@ -11,44 +11,36 @@ import javax.servlet.http.HttpServletResponse;
 import movie.dto.MovieDTO;
 import movie.service.MovieService;
 
-
 @WebServlet("/movie/detail.do")
 public class MovieDetailServlet extends HttpServlet {
-	
-	private MovieService movieService = new MovieService();
-	
-	
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException{
 
-	    
-			req.setCharacterEncoding("UTF-8");
-			resp.setContentType("text/html; charset=UTF-8");
-			
-			String kmdbMovieId = req.getParameter("movieId");
-		    String kmdbMovieSeq = req.getParameter("movieSeq");
+    private MovieService movieService = new MovieService();
 
-		    if (kmdbMovieId == null || kmdbMovieId.trim().isEmpty()
-		            || kmdbMovieSeq == null || kmdbMovieSeq.trim().isEmpty()) {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 
-		        resp.sendRedirect(req.getContextPath() + "/movie/search.do");
-		        return;
-		    }
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=UTF-8");
 
-		    MovieDTO movie = movieService.getOrSaveMovieDetail(kmdbMovieId, kmdbMovieSeq);
+        String kmdbMovieId = req.getParameter("movieId");
+        String kmdbMovieSeq = req.getParameter("movieSeq");
 
-		    if (movie == null) {
-		        req.setAttribute("errorMessage", "영화 상세 정보를 불러올 수 없습니다.");
-		        req.getRequestDispatcher("/WEB-INF/views/movie/movieList.jsp").forward(req, resp);
-		        return;
-		    }
+        if (kmdbMovieId == null || kmdbMovieId.trim().isEmpty()
+                || kmdbMovieSeq == null || kmdbMovieSeq.trim().isEmpty()) {
 
-		    req.setAttribute("movie", movie);
+            resp.sendRedirect(req.getContextPath() + "/movie/search.do");
+            return;
+        }
 
-		    req.getRequestDispatcher("/WEB-INF/views/movie/movieDetail.jsp").forward(req, resp);
-		 
-	    
+        MovieDTO movie = movieService.getOrSaveMovieDetail(kmdbMovieId, kmdbMovieSeq);
 
-	    
-	}
+        if (movie == null) {
+            req.setAttribute("errorMessage", "영화 상세 정보를 불러올 수 없습니다.");
+            req.getRequestDispatcher("/WEB-INF/views/movie/movieList.jsp").forward(req, resp);
+            return;
+        }
+
+        req.setAttribute("movie", movie);
+        req.getRequestDispatcher("/WEB-INF/views/movie/movieDetail.jsp").forward(req, resp);
+    }
 }
